47b298fd14989a3391befc54920b056d260cbbb8
Daniel Kagan <volanderon@gmail.com>
2016-03-25 16:04:03 +0300

    do my cart

--- js/directives/cart.js
+++ js/directives/cart.js
@@ -4,7 +4,31 @@ angular.module('myApp').directive('cart', function(){
        templateUrl: '/musicstore/js/directives/tpls/cart.html',
        controllerAs: 'cart',
        controller: function(){
+           var that = this;
+           this.items = [];
 
+           this.addToCart = function(album) {
+               var index = that.items.indexOf(album);
+               dd(index);
+               if (index > -1) {
+                   that.items[index].count++;
+               } else {
+                   album.count = 1;
+                   that.items.push(album);
+               }
+           };
+
+           this.total = function() {
+               var sum = 0;
+               angular.forEach(that.items, function(item) {
+                   sum += parseFloat(item.album_price) * parseInt(item.count, 10);
+               });
+               return sum;
+           };
+
+           this.clear = function() {
+               that.items = [];
+           };
        }
    };
 });
