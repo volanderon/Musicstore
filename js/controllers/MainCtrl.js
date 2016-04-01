f3a31eae086051a3a1903f0f6cbcb218c30b0c72
Daniel Kagan <volanderon@gmail.com>
2016-04-01 15:25:29 +0300

    add more functionality to home state

--- js/controllers/MainCtrl.js
+++ js/controllers/MainCtrl.js
@@ -2,4 +2,8 @@ angular.module('myApp').controller('MainCtrl', function(Cart) {
     this.getCartCount = function() {
         return Cart.getItems().length;
     };
+
+    this.addToCart = function(album) {
+        Cart.addToCart(album);
+    };
 });
