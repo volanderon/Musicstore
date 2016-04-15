8c1600f1d1cf91335a941775a36f5bcafb7e81c0
Daniel Kagan <volanderon@gmail.com>
2016-04-15 14:06:52 +0300

    prepare login services and template + remove body width styling

---
+++ js/services/Login.js
@@ -0,0 +1,5 @@
+angular.module('myApp').factory('Login', function(){
+    var service = {};
+        
+    return service;
+});
---
+++ js/services/Register.js
@@ -0,0 +1,5 @@
+angular.module('myApp').factory('Register', function(){
+    var service = {};
+
+    return service;
+});
