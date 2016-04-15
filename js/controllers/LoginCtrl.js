5c2105e5bd570b694fc64e245b094a1001f019a4
Daniel Kagan <volanderon@gmail.com>
2016-04-15 16:07:53 +0300

    do login form

--- js/controllers/LoginCtrl.js
+++ js/controllers/LoginCtrl.js
@@ -1,4 +1,11 @@
 angular.module('myApp').controller('LoginCtrl', function($uibModal) {
+    
+    var that = this;
+    this.loginInfo = {};
+    
+    this.doLogin = function() {
+        dd(that.loginInfo);
+    };
     this.createAccount = function() {
         var modalInstance = $uibModal.open({
             templateUrl: 'js/tpls/modals/create-account.html',
