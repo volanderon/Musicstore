71fd74850d57c2170d3423d2ef4d39eb6207185c
Daniel Kagan <volanderon@gmail.com>
2016-04-15 15:19:15 +0300

    add create account modal

--- js/controllers/LoginCtrl.js
+++ js/controllers/LoginCtrl.js
@@ -2,7 +2,7 @@ angular.module('myApp').controller('LoginCtrl', function($uibModal) {
     this.createAccount = function() {
         var modalInstance = $uibModal.open({
             templateUrl: 'js/tpls/modals/create-account.html',
-            // controller: 'ModalInstanceCtrl',
+            controller: 'CreateAccountCtrl as createAccount'
         });
     };
 });
